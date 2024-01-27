118c3ecb4887facdfcc573a46822a3bc5d3657db
Bruno Murua <100427363+SrZeroRequiem@users.noreply.github.com>
2024-01-27 11:38:47 +0000

    Render Poker Card

--- src/app.js
+++ src/app.js
@@ -4,8 +4,64 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+const symbolsArray = ["♦", "♥", "♠", "♣"];
+const renderBtn = document.getElementById("buttonRender");
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  renderOnLoad();
+  renderBtn.addEventListener("click", function() {
+    renderOnClick();
+  });
 };
+function randomNumber(length) {
+  let number = Math.floor(Math.random() * length);
+  return number;
+}
+function renderOnLoad() {
+  changeSymbols();
+  changeNumber();
+}
+function renderOnClick() {
+  changeSymbols();
+  changeNumber();
+  changeWidthHeight();
+}
+function changeSymbols() {
+  let symbol = symbolsArray[randomNumber(symbolsArray.length)];
+  document.getElementById("cardSymbolH").innerHTML = symbol;
+  document.getElementById("cardSymbolF").innerHTML = symbol;
+  switch (symbol) {
+    case "♦":
+    case "♥":
+      document.querySelector(".card").classList.add("text-danger");
+      break;
+    case "♠":
+    case "♣":
+      document.querySelector(".card").classList.add("text-dark");
+      break;
+  }
+}
+function changeNumber() {
+  let number = randomNumber(13);
+  switch (number) {
+    case 11:
+      number = "J";
+      break;
+    case 12:
+      number = "Q";
+      break;
+    case 13:
+      number = "K";
+      break;
+    default:
+      break;
+  }
+  document.getElementById("cardNumber").innerHTML = number.toString();
+}
+function changeWidthHeight() {
+  let widthCard = document.getElementById("width").value + "px";
+  console.log(widthCard);
+  let heightCard = document.getElementById("height").value + "px";
+  console.log(heightCard);
+  document.querySelector(".card").style.width = widthCard;
+  document.querySelector(".card").style.height = heightCard;
+}
